b72841151bb3886c261eb2f54bb6770da8595170
heather <heather.j.shane@gmail.com>
2016-03-08 15:37:41 -0500

    first commit

--- app/index.jsx
+++ app/index.jsx
@@ -1,3 +1,44 @@
 import style from './styles/';
 
-console.log('now running!')
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class MessageInput extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+
+    array: [
+    {
+      text: "hey",
+      time: 12:00,
+      user: "Charles"
+    },
+    {
+      text: "lololol",
+      time: 6:66,
+      user: "RuPaul"
+    };
+  ]
+};
+render(){
+  var messages =this.state.array.map(function(obj){
+    return (<ul>
+    <li key={obj}>{text}</li>
+    <li key={obj}>{time}</li>
+    <li key={obj}>{user}</li>
+  </ul>)
+  })
+
+  return(
+    <div>
+    {messages}
+    </div>
+
+  )
+  // var currentUser =
+  };
+};
+};
+ var mountpoint = document.querySelector("#app")
+ReactDOM.render(<MessageInput/>, mountpoint)
